45f2d970035841bac45e0ac0fa8263d9181a4db5
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-02-17 15:48:04 +0100

    buildSrc project with File.div operator

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,15 @@
+
+plugins {
+  `kotlin-dsl`
+}
+
+
+repositories {
+  gradlePluginPortal()
+  mavenCentral()
+}
+
+kotlinDslPluginOptions {
+  experimentalWarning.set(false)
+}
+
---
+++ buildSrc/src/main/kotlin/helpers.kt
@@ -0,0 +1,6 @@
+package org.jonnyzzz.cef.gradle
+
+import java.io.*
+
+operator fun File.div(s: String) = File(this, s)
+
