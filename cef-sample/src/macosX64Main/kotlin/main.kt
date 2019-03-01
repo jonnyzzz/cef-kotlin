cc0ecab47b355b76348be3706f08949c02d77216
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-03-01 21:58:18 +0100

    pass cef_string_t to the _cef_settings (with code-generation)
    Signed-off-by: Eugene Petrenko <eugene.petrenko@gmail.com>

--- cef-sample/src/macosX64Main/kotlin/main.kt
+++ cef-sample/src/macosX64Main/kotlin/main.kt
@@ -1,36 +1,36 @@
 package org.jonnyzzz.cef.example
 
-import kotlinx.atomicfu.atomic
 import kotlinx.cinterop.CPointer
-import kotlinx.cinterop.CStructVar
-import kotlinx.cinterop.NativePtr
 import kotlinx.cinterop.alloc
 import kotlinx.cinterop.allocArrayOf
 import kotlinx.cinterop.convert
 import kotlinx.cinterop.cstr
+import kotlinx.cinterop.invoke
 import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.pointed
 import kotlinx.cinterop.ptr
 import kotlinx.cinterop.sizeOf
 import kotlinx.cinterop.staticCFunction
+import org.jonnyzzz.cef.generated.copyFrom
 import org.jonnyzzz.cef.interop._cef_base_ref_counted_t
 import org.jonnyzzz.cef.interop.cef_app_t
+import org.jonnyzzz.cef.interop.cef_execute_process
 import org.jonnyzzz.cef.interop.cef_initialize
 import org.jonnyzzz.cef.interop.cef_main_args_t
 import org.jonnyzzz.cef.interop.cef_settings_t
+import org.jonnyzzz.cef.interop.cef_string_from_utf8
+import org.jonnyzzz.cef.interop.cef_string_userfree_utf16_alloc
+import org.jonnyzzz.cef.interop.cef_string_userfree_utf16_t
+import kotlin.system.exitProcess
 
 
 fun main(args: Array<String>): Unit = memScoped {
   println("CEF Kotlin sample project...")
 
-  val cefSettings = alloc<cef_settings_t> {
-
-  }
-
-  val cefArgs = alloc<cef_main_args_t> {
+  val mainArgs = alloc<cef_main_args_t> {
     this.argc = args.size
     this.argv = allocArrayOf(args.map { it.cstr.ptr })
   }
-
 
   val app = alloc<cef_app_t> {
     this.base.size = sizeOf<cef_app_t>().convert()
@@ -41,6 +41,18 @@ fun main(args: Array<String>): Unit = memScoped {
     this.base.release = staticCFunction<CPointer<_cef_base_ref_counted_t>?, Int> { 0 }
   }
 
-  cef_initialize(cefArgs.ptr, cefSettings.ptr, app.ptr, null)
+  val childProcess = cef_execute_process(mainArgs.ptr, null, null)
+  if (childProcess >= 0) {
+    exitProcess(childProcess)
+  }
+
+  val cefSettings = alloc<cef_settings_t> {
+    val str: cef_string_userfree_utf16_t? = cef_string_userfree_utf16_alloc()!!
+    val path = "/Users/jonnyzzz/Work/cef-kotlin/cef-sample/build/bin/macosX64/debugExecutable.apps/cef-sample.app/Contents/MacOS/cef-sample.kexe"
+    (cef_string_from_utf8!!)(path.cstr.ptr, path.length.convert(), str)
+    browser_subprocess_path.copyFrom(str!!.pointed)
+  }
+
+  cef_initialize(mainArgs.ptr, cefSettings.ptr, app.ptr, null)
 }
 
