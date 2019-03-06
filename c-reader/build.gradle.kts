067d676ca0a997bd062c2f834eaa855c809e0f9f
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-03-06 22:59:32 +0100

    tune ANTLR package names for the generated code

--- c-reader/build.gradle.kts
+++ c-reader/build.gradle.kts
@@ -3,26 +3,35 @@ import org.jonnyzzz.cef.gradle.div
 
 plugins {
   antlr
+  `java-library`
+  idea
   id("de.undercouch.download")
 }
 
+val packageName = "org.jonnyzzz.cef.gen.c"
+
 val grammarCcommit = "798a1f7f6dc4caa369216572186cc4c66d0da3c5"
 val grammarCUrl = "https://github.com/antlr/grammars-v4/raw/$grammarCcommit/c/C.g4"
-val grammarCdir = projectDir / "src" / "main" / "antlr" / "C"
+val grammarCdir = projectDir / "src" / "main" / "antlr" / packageName.replace(".", "/")
 val grammarCfile = grammarCdir / (grammarCUrl.split("/").last())
 
 val download = tasks.create<Download>("downloadCgrammar") {
   src(grammarCUrl)
   dest(grammarCfile)
-  overwrite(false)
 }
 
 dependencies {
   antlr("org.antlr:antlr4:4.7.2")
+  api("org.antlr:antlr4-runtime:4.7.2")
 }
 
-tasks.getByName<AntlrTask>("generateGrammarSource"){
+val generatedSrc = buildDir / "generated-src" / "antlr" / "main"
+
+val generateGrammarSource = tasks.getByName<AntlrTask>("generateGrammarSource"){
   dependsOn(download)
-  arguments.addAll(listOf("-package", "org.jonnyzzz.cef.gen.c", "-visitor", "-long-messages"))
+  arguments.addAll(listOf("-package", packageName, "-visitor", "-long-messages"))
+//  outputDirectory = generatedSrc / packageName.replace(".", "/") / "c"
 }
+
+tasks.getByName("compileJava").dependsOn(generateGrammarSource)
 
