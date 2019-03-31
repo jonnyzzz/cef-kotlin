85e05678be470d7755e791d44b0c790dffcf35e5
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-04-01 00:09:26 +0200

    wrap javadoc to make it be generated safely

--- cef-gen/src/main/kotlin/org/jonnyzzz/cef/generator/kn/KDoc.kt
+++ cef-gen/src/main/kotlin/org/jonnyzzz/cef/generator/kn/KDoc.kt
@@ -7,23 +7,39 @@ import com.squareup.kotlinpoet.TypeSpec
 interface KDocumented {
   val docComment: String?
 }
+private fun String.sanitize() = replace("%", "%%")
+        /// javadoc may contain /* or */, but Kotlin does assume is as comments,
+        /// kotlinpoet does not escape it too
+        .replace("/*", "/\u200b*")
+        .replace("*/", "*\u200b/")
 
 fun TypeSpec.Builder.addKdoc(d: KDocumented) = apply {
-  d.docComment?.let {
-    addKdoc(it)
+  try {
+    d.docComment?.let {
+      addKdoc(it.sanitize())
+    }
+  } catch (t: Throwable) {
+    throw Error("Failed to process $d. $t", t)
   }
 }
 
-
 fun PropertySpec.Builder.addKdoc(d: KDocumented) = apply {
-  d.docComment?.let {
-    addKdoc(it)
+  try {
+    d.docComment?.let {
+      addKdoc(it.sanitize())
+    }
+  } catch (t: Throwable) {
+    throw Error("Failed to process $d. $t", t)
   }
 }
 
 fun FunSpec.Builder.addKdoc(d: KDocumented) = apply {
-  d.docComment?.let {
-    addKdoc(it)
+  try {
+    d.docComment?.let {
+      addKdoc(it.sanitize())
+    }
+  } catch (t: Throwable) {
+    throw Error("Failed to process $d. $t", t)
   }
 }
 
