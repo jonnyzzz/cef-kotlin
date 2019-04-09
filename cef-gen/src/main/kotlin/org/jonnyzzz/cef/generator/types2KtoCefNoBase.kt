218553b7bf5d44c98f03489edfcd67f7d8525fa0
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-04-09 23:05:34 +0200

    reuse generated code

--- cef-gen/src/main/kotlin/org/jonnyzzz/cef/generator/types2KtoCefNoBase.kt
+++ cef-gen/src/main/kotlin/org/jonnyzzz/cef/generator/types2KtoCefNoBase.kt
@@ -2,8 +2,10 @@ package org.jonnyzzz.cef.generator
 
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.MemberName
 import com.squareup.kotlinpoet.ParameterSpec
+import com.squareup.kotlinpoet.UNIT
 
 
 private val KNSimplePublicField.tmpFieldName
@@ -11,12 +13,40 @@ private val KNSimplePublicField.tmpFieldName
 
 fun generateWrapCefToKNoBase(file : FileSpec.Builder, info: KNSimpleTypeInfo, mapper: CefTypeMapperGenerator): Unit = info.run {
   FunSpec.builder(wrapKtoCefPointerName).apply {
+    addModifiers(KModifier.PRIVATE)
+    returns(UNIT)
+    receiver(rawStruct)
+    addParameter(ParameterSpec.builder("cefStruct", kInterfaceTypeName).build())
+
+    addStatement("%M(%M, 0, %T.size.%M())", fnPosixMemset, fnPtr, rawStruct, fnConvert)
+
+    //cases
+    // - primitive type => assign
+    // - CefRefCounted => class wrap
+    // - _cef_string_^ => copyFrom()
+
+    for (p in info.fields) {
+      addCode(mapper.mapTypeFromKToCefCode(p, "cefStruct.${p.kFieldName}", p.tmpFieldName))
+    }
+
+    for (p in info.fields) {
+      when (p.cReturnType) {
+        cefString16,
+        cefString16.asNullableCPointer() ->
+          addStatement("%M(this::${p.cFieldName}, ${p.tmpFieldName})", MemberName("org.jonnyzzz.cef", "copyCefString"))
+
+        else ->
+          addStatement("${p.cFieldName} = ${p.tmpFieldName}")
+      }
+    }
+  }.build().also { file.addFunction(it) }
+
+  FunSpec.builder(wrapKtoCefPointerName).apply {
     returns(rawStruct.asCPointer())
     receiver(memberScopeType)
     addParameter(ParameterSpec.builder("cefStruct", kInterfaceTypeName).build())
 
-    returnStruct(fnAlloc, info, mapper)
-    addStatement("return kResult.ptr")
+    addStatement("return %M<%T>{ $wrapKtoCefPointerName(cefStruct) }.ptr", fnAlloc, rawStruct)
   }.build().also { file.addFunction(it) }
 
 
@@ -24,37 +54,7 @@ fun generateWrapCefToKNoBase(file : FileSpec.Builder, info: KNSimpleTypeInfo, ma
     returns(rawStruct.asCValue())
     addParameter(ParameterSpec.builder("cefStruct", kInterfaceTypeName).build())
 
-    returnStruct(fnCValue, info, mapper)
-    addStatement("return kResult")
+    addStatement("return %M<%T>{ $wrapKtoCefValueName(cefStruct) }", fnCValue, rawStruct)
 
   }.build().also { file.addFunction(it) }
-}
-
-private fun FunSpec.Builder.returnStruct(fnFactory: MemberName,
-                                         info: KNSimpleTypeInfo,
-                                         mapper: CefTypeMapperGenerator) = info.run {
-  beginControlFlow("val kResult = %M<%T>", fnFactory, rawStruct)
-
-  addStatement("%M(%M, 0, %T.size.%M())", fnPosixMemset, fnPtr, rawStruct, fnConvert)
-
-  //cases
-  // - primitive type => assign
-  // - CefRefCounted => class wrap
-  // - _cef_string_^ => copyFrom()
-  for (p in info.fields) {
-    addCode(mapper.mapTypeFromKToCefCode(p, "cefStruct.${p.kFieldName}", p.tmpFieldName))
-  }
-
-  for (p in info.fields) {
-    when (p.cReturnType) {
-      cefString16,
-      cefString16.asNullableCPointer() ->
-        addStatement("%M(this::${p.cFieldName}, ${p.tmpFieldName})", MemberName("org.jonnyzzz.cef", "copyCefString"))
-
-      else ->
-        addStatement("${p.cFieldName} = ${p.tmpFieldName}")
-    }
-  }
-
-  endControlFlow()
 }
